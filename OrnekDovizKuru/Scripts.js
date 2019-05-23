29a9646230794a383d7d5a98fd6d932b9f396463
sc302 <ibrahim.can818@gmail.com>
2019-05-23 15:40:33 +0300

    json okuma

--- OrnekDovizKuru/Scripts.js
+++ OrnekDovizKuru/Scripts.js
@@ -2,13 +2,16 @@
 
 $(document).ready(function () {
 
+  //https://www.doviz.gen.tr/doviz_json.asp
+   
+  //https://gist.githubusercontent.com/cbmgit/852c2702d4342e7811c95f8ffc2f017f/raw/InsuranceCompanies.json
 
     $('#btn5').click(function(){
         var items = [];
-        $.getJSON('https://www.doviz.gen.tr/doviz_json.asp',function(data)
+        $.getJSON('https://gist.githubusercontent.com/cbmgit/852c2702d4342e7811c95f8ffc2f017f/raw/InsuranceCompanies.json',function(data)
         {
-            $.each( data, function( key, val ) {
-                items.push( "<li id='" + key + "'>" + val + "</li>" );
+            $.each( data["InsuranceCompanies"]["Top Insurance Companies"], function( key, val ) {
+                items.push( "<li id='" + key + "'>" + val["Market Capitalization"] + "</li>" );
               });
               $( "<ul/>", {
                 "class": "my-new-list",
@@ -19,3 +22,15 @@ $(document).ready(function () {
 
   
 });
+
+/*
+ 
+
+    $('#btn5').click(function(){
+        var items = [];
+        $.getJSON('https://gist.githubusercontent.com/cbmgit/852c2702d4342e7811c95f8ffc2f017f/raw/InsuranceCompanies.json',function(data)
+        {
+            console.log(data["InsuranceCompanies"]["Top Insurance Companies"]);
+        });
+    }); 
+    */
